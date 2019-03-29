001ae6009adf979801f5c9c1f3572d5d892036bc
DEMI0815 <807205251@qq.com>
2019-03-29 11:24:34 +0800

    update the class of InverseReduce

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -16,6 +15,14 @@ public class InverseReduce {
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>();
+        int randomNumber = random.nextInt(3);
+
+        while(number - randomNumber >=0){
+            number = number - randomNumber;
+            list.add(number);
+        }
+
+        return  list;
     }
 }
